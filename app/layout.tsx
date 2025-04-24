422a65ab2bdf6d36d10ff56eb0b57ff4ef039403
Ronald Burns <7757589+ronaldburns@users.noreply.github.com>
2025-04-24 01:12:33 -0500

    Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Tee Executive Portfolio',
+    template: '%s | Tee Executive Portfolio',
   },
   description: 'This is my portfolio.',
   openGraph: {
